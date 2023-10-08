475a2d24feb1c4a6efddefe3e26a276759bd9822
Liedson Bielisque <137654757+Bielisque@users.noreply.github.com>
2023-10-08 18:00:55 -0300

    content created

---
+++ src/app/pages/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "BESOURO AZUL",
+    "description": "Bruna Marquezine começou bem sua carreira em Hollywood com Besouro Azul, novo filme de super herói baseado nos quadrinhos da DC que estreia nesta quinta-feira (17) nos cinemas brasileiros.",
+    "photoCover":"https://portalpopline.com.br/wp-content/uploads/2023/07/bruna-marquezine-besouro-azul-capa.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada na DC Comics",
+    "description": "bla blabla",
+    "photoCover":"https://uploads.jovemnerd.com.br/wp-content/uploads/2022/01/superman-lois-dc-melhor-versao-moderna.jpg"
+  },
+  {
+    "id":"3",
+    "title": "BESOURO AZUL",
+    "description": "Novo filme do Shazam será lançado em março no Brasil.",
+    "photoCover":"https://gkpb.com.br/wp-content/uploads/2023/01/shazam-furia-dos-deuses-novo-trailer-scaled.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Nova Série anunciada na DC Comics",
+    "description": "Novo filme da DC Comics Coringa 2 está em pós produção, afirma diretor",
+    "photoCover":"https://uploads.jovemnerd.com.br/wp-content/uploads/2023/06/coringa_2_joaquin_gaga__ayn496-760x428.jpg"
+  }
+]
